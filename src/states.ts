d64672cb0967a3ba6136738a334c637b66f473ee
BaBaSaMa <julong170501@gmail.com>
2026-05-12 16:34:52 +0800

    added screen transition effect, with default to app icon image

--- src/states.ts
+++ src/states.ts
@@ -1,4 +1,5 @@
 import type { App } from "@model/app";
+import type { GenieTransition } from "@model/GeineTransition";
 
 import { atom } from "nanostores";
 
@@ -7,3 +8,5 @@ export const selected_app = atom<string | undefined>(undefined);
 export const apps = atom<App[]>([]);
 
 export const app_memory = atom<string[]>([]);
+
+export const transition_screen = atom<GenieTransition | undefined>(undefined); 
